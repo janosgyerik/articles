ee72808b297904a5f1897e4a0e78cfcb061dda7f
Janos Gyerik <info@janosgyerik.com>
2013-06-16 19:05:48 +0300

    Update chart-repos.js

--- cool-features-of-github/chart-repos.js
+++ cool-features-of-github/chart-repos.js
@@ -1,13 +1,7 @@
+// Paste this code on this page:
+// https://code.google.com/apis/ajax/playground/?type=visualization#area_chart
 function drawVisualization() {
-  // Some raw data (not necessarily accurate)
-/*
-- Jan 2008 -- 0
-- Dec 2008 -- 42k users, 80k repos
-- 2010 -- 166k users, 484k repos
-- 2011 -- 510k users, 1.3m repos
-- 2012 -- 1.2m users, 3.4m repos
-- Aug 2012 -- 1.9m users, 6.5m repos
-*/
+  // Some raw data
   var data = google.visualization.arrayToDataTable([
     ['Year',   'Users' ],
     ['2008',    0 ],

7874299626c744c2cbb57b9cf2fb42c5790a183d
Janos Gyerik <janos@xw8400>
2013-09-30 00:21:41 +0200

    updated markdown generator

--- hacking-contest-lessons/gen.sh
+++ hacking-contest-lessons/gen.sh
@@ -7,5 +7,5 @@ prefix=security-lessons
 mkdir -p $out
 
 cat intro.md setup.md level?.md conclusion.md > $out/$prefix.md
-cat intro.md setup.md level?.md conclusion.md | markdown_py > $out/$prefix.html
+cat intro.md setup.md level?.md conclusion.md | sed -e '/```/,/```/ s/^/    /' -e 's/^    ```//' | markdown_py > $out/$prefix.html
 html2text -style pretty $out/$prefix.html > $out/$prefix.txt

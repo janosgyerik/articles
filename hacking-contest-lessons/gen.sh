5bbfb5f7356275853c930e83aa7d765c7fa5e536
Janos Gyerik <janos@xw8400>
2013-09-29 23:52:57 +0200

    added script to generate all formats

---
+++ hacking-contest-lessons/gen.sh
@@ -0,0 +1,11 @@
+#!/bin/sh -x
+
+cd $(dirname "$0")
+
+out=out
+prefix=security-lessons
+mkdir -p $out
+
+cat intro.md setup.md level?.md conclusion.md > $out/$prefix.md
+cat intro.md setup.md level?.md conclusion.md | markdown_py > $out/$prefix.html
+html2text -style pretty $out/$prefix.html > $out/$prefix.txt
